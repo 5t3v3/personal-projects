646894da07a5407bd1c5ac09e331d2bfbb2a66d4
root <root@localhost.localdomain>
2020-06-11 18:19:07 +0530

    ordered

--- recon.sh
+++ recon.sh
@@ -4,10 +4,24 @@ mkdir $1
 cd $1
 echo "Finding subdomains for " $1
 
-echo $1 | assetfinder --subs-only > $1.asset.txt | amass enum --passive -d $1 > $1.amass.txt  | sublist3r  -d $1  -o $1.subl.txt
+# Subdomain Enumeration
+
+assetfinder --subs-only $1  > $1.asset.txt 
+amass enum --passive -d $1 > $1.amass.txt 
+sublist3r  -d $1  -o $1.subl.txt
+
+#Removing duplicates and sorting
 cat $1.asset.txt $1.subl.txt $1.amass.txt | sort | uniq > subdomain.txt
 echo "Running httprobe for active subdomains"
+
+#Looking for active domains
 cat subdomain.txt | httprobe > httprobe.txt
+
+#Screenshots
 echo "screenshots for active domains"
-python3 /root/tools/EyeWitness/EyeWitness.py --no-prompt  -f httprobe.txt --web -d eye
+python3 /root/tools/EyeWitness/EyeWitness.py --no-prompt  -f httprobe.txt --web --timeout 14 -d eye
+
+#Subdomain takeover
+#./root/go_projects/src/github.com/Ice3man543/subover -l subdomain.txt -v > subtakeover.txt
+cat subdomain.txt | xargs -n 1 -I{} host -t CNAME {}  >cname.txt
 
